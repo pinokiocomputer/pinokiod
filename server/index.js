6bc772f2931a4da63cab2f8a75b0a52ee8d5e8d4
cocktailpeanut <cocktailpeanuts@proton.me>
2023-10-03 13:27:11 -0400

    remove torch from default

--- server/index.js
+++ server/index.js
@@ -320,10 +320,10 @@ class Server {
           type: "bin",
           name: "git",
           uri: "git"
-        }, {
-          type: "bin",
-          name: "torch",
-          uri: "torch"
+//        }, {
+//          type: "bin",
+//          name: "torch",
+//          uri: "torch"
         }]
         console.log("Resolved", resolved)
 
