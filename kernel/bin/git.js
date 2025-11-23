5af611a763960eadf1d250c0283f53b96b40de83
cocktailpeanut <cocktailpeanuts@proton.me>
2025-11-23 17:19:51 -0500

    git version fix
    make sure it installs 2.51.0 (not old version), and make sure to uninstall git-bash if it was already installed.

--- kernel/bin/git.js
+++ kernel/bin/git.js
@@ -1,21 +1,25 @@
 const fs = require('fs')
 const _7z = require('7zip-min-win-asar-support');
 const fetch = require('cross-fetch')
+const semver = require('semver')
 const { rimraf } = require('rimraf')
 const path = require("path")
 class Git {
   cmd() {
     if (this.kernel.platform === "darwin") {
-      return "git git-lfs gh=2.82.1"
+      return "git=2.51.0 git-lfs gh=2.82.1"
     } else if (this.kernel.platform === "win32") {
       //return "git git-lfs gh=2.82.1 git-bash"
-      return "git git-lfs gh=2.82.1 m2-base"
+      return "git=2.51.0 git-lfs gh=2.82.1 m2-base"
     } else {
-      return "git git-lfs gh=2.82.1"
+      return "git=2.51.0 git-lfs gh=2.82.1"
     }
   }
   async install(req, ondata) {
     console.log("GIT install")
+    if (this.kernel.platform === "win32") {
+      await this.kernel.bin.exec({ message: "conda remove git-bash" }, ondata)
+    }
     await this.kernel.bin.exec({
       message: [
         "conda clean -y --all",
@@ -24,6 +28,16 @@ class Git {
     }, ondata)
   }
   async installed() {
+    let version = this.kernel.bin.installed.conda_versions.git
+    console.log("git version", version)
+    let coerced = semver.coerce(version)
+    console.log("git coerced", coerced)
+    let requirement = ">=2.51.0"
+    let satisfied = semver.satisfies(coerced, requirement)
+    console.log("git version satisfied?", satisfied)
+    if (!satisfied) {
+      return false 
+    }
     if (this.kernel.platform === "darwin") {
       return this.kernel.bin.installed.conda && this.kernel.bin.installed.conda.has("git") && this.kernel.bin.installed.conda.has("gh")
     } else if (this.kernel.platform === "win32") {
