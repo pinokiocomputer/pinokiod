79ca767359cd503d1fd90a17e6850aa6880b3e88
cocktailpeanut <cocktailpeanuts@proton.me>
2026-06-19 23:40:42 -0400

    Fix sidebar peer ordering

---
+++ test/main-sidebar.test.js
@@ -0,0 +1,20 @@
+const assert = require('node:assert/strict')
+const fs = require('node:fs/promises')
+const path = require('node:path')
+const test = require('node:test')
+
+const sidebarFile = path.resolve(__dirname, '..', 'server', 'views', 'partials', 'main_sidebar.ejs')
+
+test('main sidebar renders local network peers before phone access', async () => {
+  const source = await fs.readFile(sidebarFile, 'utf8')
+
+  const localNetworkIndex = source.indexOf('Local network')
+  const peerListIndex = source.indexOf('sidebarList.forEach')
+  const phoneIndex = source.indexOf('main-sidebar-phone-trigger')
+
+  assert.notEqual(localNetworkIndex, -1)
+  assert.notEqual(peerListIndex, -1)
+  assert.notEqual(phoneIndex, -1)
+  assert.ok(localNetworkIndex < peerListIndex)
+  assert.ok(peerListIndex < phoneIndex)
+})
